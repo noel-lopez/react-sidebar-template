f000a11e0fe739270e47675790d6ab14e93e1d7a
noel-lopez <nlopez.publi@gmail.com>
2022-12-20 18:01:26 +0100

    added sidebar page links

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: 90px auto;
   background: ${({theme}) => theme.bgtotal};
+  transition: all 0.3s;
   &.active {
     grid-template-columns: 300px auto;
   }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,14 @@
 import styled from 'styled-components'
 import logo from '../assets/react.svg'
 import {v} from '../styles/Variables'
-import {AiOutlineLeft, AiOutlineRight} from 'react-icons/ai'
+import {
+  AiOutlineLeft, 
+  AiOutlineHome, 
+  AiOutlineShopping, 
+  AiOutlineUser, 
+  AiOutlineBarChart
+} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
 
 export function Sidebar({ open, setOpen }) {
   const swapOpen = () => {
@@ -21,10 +28,55 @@ export function Sidebar({ open, setOpen }) {
           sidebar
         </h2>
       </div>
+      {linksArray.map(({icon, label, to}) => (
+        <div className='linkContainer' key={label}>
+          <Link to={to} className='links'>
+            <div className='linkIcon'>
+              {icon}
+            </div>
+            {
+              open && (
+                <span>{label}</span>
+              )
+            }
+          </Link>
+        </div>
+      ))}
     </Container>
   )
 }
 
+//#region Data Links
+const linksArray = [
+  {
+    label: 'Home',
+    icon: <AiOutlineHome/>,
+    to: '/'
+  },
+  {
+    label: 'Products',
+    icon: <AiOutlineShopping />,
+    to: '/products'
+  },
+  {
+    label: 'Users',
+    icon: <AiOutlineUser />,
+    to: '/users'
+  },
+  {
+    label: 'Diagrams',
+    icon: <AiOutlineBarChart />,
+    to: '/products'
+  },
+  {
+    label: 'Stats',
+    icon: <AiOutlineBarChart />,
+    to: '/products'
+  }
+]
+//#endregion
+
+//#region STYLED COMPONENTS
 const Container = styled.div`
   background: ${(props) => props.theme.bg};
   color: ${(props) => props.theme.text};
@@ -73,4 +125,25 @@ const Container = styled.div`
       display: ${({isOpen}) => (isOpen?`block`:`none`)};
     }
   }
+  .linkContainer {
+    margin: 8px 0;
+    padding: 0 15%;
+    :hover{
+      background: ${(props) => props.theme.bg3};
+    }
+    .links {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      padding: calc(${v.smSpacing}-2px) 0;
+      .linkIcon {
+        padding: ${v.smSpacing} ${v.mdSpacing};
+        display: flex;
+        svg {
+          font-size: 25px;
+        }
+      }
+    }
+  }
 `
+//#endregion
