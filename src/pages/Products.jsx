e19ded379e1d6cdfa561610ad2bff6ef3dcb9b8e
noel-lopez <nlopez.publi@gmail.com>
2022-12-20 15:39:56 +0100

    basic pages router with react-router-dom

---
+++ src/pages/Products.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+export function Products() {
+  return (
+    <Container>
+      <h1>Products</h1>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+
+`
---
+++ src/routers/routes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Home} from '../pages/Home'
+import {Products} from '../pages/Products'
+import {Users} from '../pages/Users'
+
+export function MyRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/products' element={<Products />}/>
+        <Route path='/users' element={<Users />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
